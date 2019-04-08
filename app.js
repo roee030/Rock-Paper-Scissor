bdf9c706004aec52f7be5a865d8696148d3a83b4
roee030 <roee.angel93@gmail.com>
2019-04-08 15:29:28 +0300

    more function and scripts
    I need win, lose and draw function

---
+++ app.js
@@ -0,0 +1,22 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelectorAll(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice()
+{
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function main()
+{
+    console.log(getComputerChoice());
+}
+main();
