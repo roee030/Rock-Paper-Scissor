e580ac357378a557cf1efc3b091a7a0c83add88d
roee030 <roee.angel93@gmail.com>
2019-04-12 12:51:47 +0300

    add dialog box
    needs to finish the play box

--- app.js
+++ app.js
@@ -3,16 +3,42 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelectorAll(".result");
+const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+var span = document.getElementsByClassName(".close-button")[0];
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
 
 function getComputerChoice()
 {
     const choices = ['r','p','s'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
+}
+function win(userChoice,computerChoice)
+{
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = userChoice + " beats " + computerChoice + ". You WIN!";
+}
+function lose(userChoice,computerChoice)
+{
+    console.log("lose");
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = computerChoice + " beats " + userChoice + ". You LOSE!";
 }
 function game(userChoice)
 {
@@ -22,12 +48,12 @@ function game(userChoice)
         case "rp":
         case "pr":
         case "sp":
-            console.log("USER WIN!");
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            console.log("USER LOSE!");
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
@@ -38,6 +64,7 @@ function game(userChoice)
 }
 function main()
 {
+    toggleModal();
     rock_div.addEventListener('click',function()
     {
         game("r");
